2ef8d19497eea274baa2b833b873a35792c66961
Dmitry Gerasimov <nsmg@yandex.ru>
2021-01-02 22:41:55 +0300

    implementing MENU interface

--- x_zero.py
+++ x_zero.py
@@ -1,13 +1,36 @@
 from description import description
 from rules import rules
-from managment import managment
 from menu import menu
 import subprocess
 
 
-clear = lambda: subprocess.call('cls||clear', shell=True)
+clear = lambda: subprocess.call('cls||clear', shell=True) # функция очистки консоли экрана
 clear()
-description()
-rules()
-managment()
-menu()
+
+
+def wrong():
+    print("-----------------------------------------")
+    print(" Вы ввели некорректный номер пункта меню ")
+
+
+def menuloop():
+    while True:
+        mode = menu()
+        clear()
+        if mode == "1":
+            description()
+            clear()
+        elif mode == "2":
+            rules()
+            clear()
+        elif mode == "3":
+            
+            clear()
+        elif mode == "4":
+            break
+        else:
+            wrong()
+
+menuloop()
+
+
